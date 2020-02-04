b2e5b327f1300cd693dd0b7e643756d272c5cfea
Maulana <mprambadi@gmail.com>
2020-02-04 16:56:26 +0700

    add env example

--- config/database.js
+++ config/database.js
@@ -1,25 +1,27 @@
+require("dotenv").config();
+
 module.exports = {
-  "development": {
-    "username": "maul",
-    "password": "maul",
-    "database": "express_graphql",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
   }
-}
+};
